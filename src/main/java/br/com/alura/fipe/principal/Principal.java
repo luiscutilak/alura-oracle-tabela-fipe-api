056d06e56aee8143eab2fbcd3d7f26272c969032
Luis Fernando Cutilak <luiscutilak@gmail.com>
2024-06-06 23:03:46 -0300

    feat: Finalizado projeto Fipe, manipulando dados da Api, e melhorando a experiencia do usuario.

--- src/main/java/br/com/alura/fipe/principal/Principal.java
+++ src/main/java/br/com/alura/fipe/principal/Principal.java
@@ -2,11 +2,15 @@ package br.com.alura.fipe.principal;
 
 import br.com.alura.fipe.model.Dados;
 import br.com.alura.fipe.model.Modelos;
+import br.com.alura.fipe.model.Veiculo;
 import br.com.alura.fipe.service.ConsumoApi;
 import br.com.alura.fipe.service.ConverteDados;
 
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -56,5 +60,32 @@ public class Principal {
                 .sorted(Comparator.comparing(Dados::codigo))
                 .forEach(System.out::println);
 
+        System.out.println("\nDigite um trecho do nome do carro a ser buscado: ");
+        var nomeVeiculo = sc.nextLine();
+
+        List<Dados> modelosFiltrados = modeloLista.modelos().stream()
+                .filter(m -> m.nome().toLowerCase().contains(nomeVeiculo.toLowerCase()))
+                .collect(Collectors.toList());
+        System.out.println("\nModelos filtrados:");
+        modelosFiltrados.forEach(System.out::println);
+
+        System.out.println("Agora, digite por favor o código do modelo: ");
+        var codigoModelo = sc.nextLine();
+
+        endereco = endereco + "/" + codigoModelo + "/anos";
+        json = c.obterDados(endereco, Modelos.class);
+        List<Dados> anos = conversor.obterLista(json, Dados.class);
+
+        List<Veiculo> veiculos = new ArrayList<>();
+
+        for (int i = 0; i < anos.size(); i++) {
+            var enderecoAnos = endereco + "/" + anos.get(i).codigo();
+            json = c.obterDados(enderecoAnos, Modelos.class);
+            Veiculo veiculo = conversor.obterDados(json, Veiculo.class);
+            veiculos.add(veiculo);
+        }
+
+        System.out.println("\nTodos os veículos filtrados com avalições por ano: ");
+        veiculos.forEach(System.out::println);
     }
 }
